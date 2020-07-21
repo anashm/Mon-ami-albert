cbf72dec4fe0faa47301008d95028ac1d9f3d272
Nebur242 Nzaou <rm.nzaou.etudes@gmail.com>
2020-07-21 17:50:34 +0100

    login page finished

--- src/pages/Login/Social/Facebook/Facebook.js
+++ src/pages/Login/Social/Facebook/Facebook.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
 
 const Facebook = () => {
+
+    const appID = "1088597931155576"
+
+    const responseFacebook = (response) => console.log(response);
+    
+
+
     return (
-        <div>
-            Facebook btn
-        </div>
+        <FacebookLogin
+        appId = { appID }
+        fields="name,email,picture"
+/*         onClick={componentClicked}
+ */     callback={responseFacebook} 
+        cssClass="facebook-login-btn"
+        icon="fa-facebook"
+        textButton = 'Se connecter avec Facebook'
+        />
     )
 }
 
