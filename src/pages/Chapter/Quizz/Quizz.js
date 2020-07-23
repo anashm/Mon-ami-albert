34670a1845f3e9762c04692fd6cfb3e8ea1c3b3e
Nebur242 Nzaou <rm.nzaou.etudes@gmail.com>
2020-07-23 22:24:04 +0100

    worked on quizz logic and page

--- src/pages/Chapter/Quizz/Quizz.js
+++ src/pages/Chapter/Quizz/Quizz.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import { Divider } from 'semantic-ui-react'
+import Progress from './Progress/Progress';
+import DownloadButton from './DownloadButton/DownloadButton';
+import QuizzForm from './QuizzForm/QuizzForm';
+
+import './Quizz.scss';
+
+
 
 const Quizz = () => {
     return (
-        <div>
-            Quizz
+        <div className = 'quizz-container'>
+            <div className="container">
+                <Progress/>
+                <Divider hidden />
+                <DownloadButton/>
+                <Divider hidden />
+                <Divider hidden />
+                <QuizzForm multiple />
+                <Divider hidden />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Quizz
+export default Quizz;
