e9adc439f53d85fafef8e36e19e5005a6fcdbb6b
unknown <anashmidouch@gmail.com>
2020-08-31 18:00:47 +0200

    z index page chapitre

--- src/components/header/Classement/ClassementGeneral.js
+++ src/components/header/Classement/ClassementGeneral.js
@@ -12,46 +12,47 @@ const ClassementGeneral = () => {
     const history = useHistory();
 
     const [ progression , setProgression ] = useState([])
-
+   
     useEffect(() => {
-
-        
-
+       
         firebase.auth.onAuthStateChanged( user => {
-            if(user){
-                //code if realod page pour garder context api values
-                userContext.get_connected_user(user);
-                const userId = user.uid;                      
-                const database = firebase.getData();
-                const reference =  database.ref('users/'+userId)
-
-                
-
+          if(user){
+              //code if realod page pour garder context api values
+               userContext.get_connected_user(user);
+               const userId = user.uid;                      
+               const database = firebase.getData();
+               const reference =  database.ref('users/'+userId)
+      
                 reference.once("value", user_informations => {
-                    userContext.get_user_informations(user_informations.val());
-                    setProgression(user_informations.val().Progression)
-                    //console.log(user_informations.val().Progression)
-                })
-            }
-            else{
-                console.log('not login');
-                history.push('/')
-            }
+                userContext.get_user_informations(user_informations.val());
+
+                    setProgression(user_informations.val().Progression.Seconde.Mathematique)
+                     const collection = user_informations.val().Progression             
+                     console.log(collection)
+                }) 
+                     
+          }
+          else{
+
+           console.log('not login');
+           history.push('/')
+          }
         });
-    }, []);
+  
+      }, []);
 
     return (
         <div>
             <p>this is classement général</p>
             <ul>
-                {progression ? console.log(progression.Seconde) : console.log('walo')}
                
-                {/* {progression.map( (progression , index) => {
+               
+                {/* {progression ? progression.map( (progress , index) => {
                     return (
-                        <li> hello </li>
+                        <li key={index}> {progress} </li>
                     )
                     
-                }) } */}
+                }) : '' } */}
             </ul>
         </div>
     )
