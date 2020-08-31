4ad414f48e488027b2804f3a450ef0fa98159475
unknown <anashmidouch@gmail.com>
2020-08-31 15:14:29 +0200

    new menu

---
+++ src/components/header/Classement/ClassementGeneral.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClassementGeneral = () => {
+    return (
+        <div>
+            <p>this is classement général</p>
+        </div>
+    )
+}
+
+export default ClassementGeneral;
