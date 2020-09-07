37217adf2ab668dbc72defb1da6ddaa260491a86
unknown <anashmidouch@gmail.com>
2020-09-07 18:24:35 +0200

    animation lottie not found

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useLottie } from "lottie-react";
+import './NotFound.css'
+
+import newphoneLottie from '../../animation/notFound/oops.json';
 
 const NotFound = () => {
+    const [ options , setOptions ] = useState({
+        animationData: newphoneLottie,
+        loop: true,
+        autoplay: true
+    });
+    
+
+    
+
+    const { View } = useLottie(options);
+
     return (
-        <section className = 'not-found-container'>
-            <div className="container">
-                <p> Not found </p>
+        <section className = 'not-found-container' >
+            <div className="not_found_container">
+                
+                <p className="svg_container_not_found"> {View} </p>
+                
+            </div>
+            <div className="title_not_found">
+                <h2 >Nous n'avons pas trouvé ce que vous recherchiez.</h2>
             </div>
         </section>
     )
