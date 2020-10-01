d4fb0b9f2dce9da4db0b8a7fe9648dabc11660c2
unknown <anashmidouch@gmail.com>
2020-10-01 16:53:45 +0200

    scan qr code plus points

--- src/pages/QRCode/QRComponent.js
+++ src/pages/QRCode/QRComponent.js
@@ -1,33 +1,86 @@
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import QrReader from 'react-qr-reader'
+import UserContext from '../../Context/UserContext/UserContext';
+import { Button } from 'semantic-ui-react';
+import {FirebaseContext} from '../../firebase';
 
 const QRComponent = () => {
-    const [result , setResult] = useState(null);
+    const userContext = useContext(UserContext)
+    const firebase = useContext(FirebaseContext)
+
+
     const[idCode,setIdCode] = useState('');
     const[nbrPoints,setNbrPoint] = useState(0);
+    const[showScanner , setShowScanner] = useState(false);
+
+   
+    
 
     const handleScan = data => {
+      
         if (data) {
-          setResult(data)
-          let resultat = data.split('###');
-          setIdCode(resultat[0]);
-          setNbrPoint(resultat[1]);
+          if(userContext){
+            const user =  userContext.user; 
+            const userId = user.uid;  
+            
+            const database = firebase.getData();
+            const reference =  database.ref('users/')
+            const user_points = userContext.user_informations.points
+            let resultat = data.split('###');
+            setIdCode(resultat[0]);
+            setNbrPoint(resultat[1]);
+
+              reference.child(userId).update({
+                  code_scanned : resultat[0],
+                  points : user_points + resultat[1]
+                }).then(() => {
+                  userContext.update_user_points_anas(user_points + resultat[1]);
+                    alert('succes');
+                })
+    
+          }
+          else{
+            alert('vous devez vous connectez dabord !')
+          }
+            
+
+
+         // }
+        
+          
         }
       }
     const handleError = err => {
         console.error(err)
      
       }
+    const HandleShowScanner = () => {
+      setShowScanner(true);
+    }
     return (
         <div>
-            <QrReader
+          <Button onClick={HandleShowScanner}  >Scanner</Button>
+         
+          {
+            showScanner ? 
+              <QrReader
                 delay={300}
                 onError={handleError}
                 onScan={handleScan}
                 style={{ width: '100%' }}
                 />
+                :
+                ''
+          }
+            
             <p>{idCode}</p>
-            <p>{nbrPoints}</p>
+            {
+              nbrPoints > 0 ?
+                <p>{nbrPoints}</p>
+
+              : ''
+            }
+            
         </div>
     )
 }
