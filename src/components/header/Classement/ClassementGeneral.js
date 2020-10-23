86d7eb90ac81122596b87bdad7a29d090d1e7f43
unknown <anashmidouch@gmail.com>
2020-10-24 00:32:06 +0200

    classement lycee par pays general

--- src/components/header/Classement/ClassementGeneral.js
+++ src/components/header/Classement/ClassementGeneral.js
@@ -7,6 +7,7 @@ import './ClassementGeneral.css';
 import { Dimmer, Loader } from 'semantic-ui-react';
 import highfive from '../../../images/highFive/HIGHFIVE.svg';
 import Header from '../../header/Header';
+import { Tab,Tabs } from 'react-bootstrap';
 
 const ClassementGeneral = () => {
 
@@ -18,6 +19,8 @@ const ClassementGeneral = () => {
     const [dimmer , setDimmer ] = useState(true);
     const [level , setLevel] = useState('');
 
+    const[eleveMaroc,setEleveMaroc] = useState([])
+    const[eleveFrance,setEleveFrance] = useState([])
     useEffect(() => {
         if(userContext.user && userContext.user_informations){
             //code if realod page pour garder context api values
@@ -26,21 +29,23 @@ const ClassementGeneral = () => {
             //const reference_user = database.ref('users/'+userId);
             /*  reference_user.once("value", user_informations => {
                 userContext.get_user_informations(user_informations.val()); */
-                
+                let resultat_pays ={}  
             const reference =  database.ref('users');
             reference.once("value", snapshot => {
-                let object_final = []
+                let object_final_maroc = []
+                let object_final_france = [];
                 
+
                 snapshot.forEach(function(childSnapshot) {
                   
                     const childData = childSnapshot.val();
                    
                     setLevel(userContext.user_informations.level)
 
-                    if(childData.points)
-                    if(childData.level === userContext.user_informations.level)
-                    object_final = [
-                        ...object_final,
+                    if(childData.points && childData.pays)
+                    if(childData.level === userContext.user_informations.level && childData.pays.toLowerCase() === 'ma')
+                    object_final_maroc = [
+                        ...object_final_maroc,
                         {
                             points : childData.points,
                             firstName : childData.firstName,
@@ -50,12 +55,43 @@ const ClassementGeneral = () => {
                         }
                     ]
 
-                    object_final.sort((a, b) => b.points - a.points );
-                    setClassement(object_final)
+                    if(childData.points && childData.pays)
+                    if(childData.level === userContext.user_informations.level && childData.pays.toLowerCase() === 'fr')
+                    object_final_france = [
+                        ...object_final_france,
+                        {
+                            points : childData.points,
+                            firstName : childData.firstName,
+                            lastName : childData.lastName,
+                            etablissement : childData.etablissement
+                            
+                        }
+                    ]
+                    
+                      object_final_maroc.sort((a, b) => b.points - a.points );
+                      object_final_france.sort((a, b) => b.points - a.points );
+                    //setClassement(object_final)
                 });
+                resultat_pays = {
+                    maroc : object_final_maroc,
+                    france : object_final_france
+                  }
+                 
             }).then( () => {
                 console.log('done');
                 setDimmer(false);
+                return new Promise(function(resolve,reject){
+                    if(resultat_pays.maroc || resultat_pays.france){
+                      resolve(resultat_pays)
+                    }
+                    else 
+                      reject('Il y a aucun lycée')
+                  })
+               
+            }).then((data)=>{
+                setEleveFrance(data.france)
+                setEleveMaroc(data.maroc)
+                console.log(data)
             }).catch(e => {
                 console.log(e);
                 history.push('/404');
@@ -73,45 +109,99 @@ const ClassementGeneral = () => {
             <h3>Classement Général { level ? level : ''} </h3> 
 
             
+            <Tabs defaultActiveKey="Lycées du Maroc" id="uncontrolled-tab-example">
+                <Tab eventKey="Lycées de France" title="Lycées de France">
+                    
 
-            <Table unstackable>
-                <Table.Body>
+                <Table unstackable>
+                        <Table.Body>
 
-               {
-                   classement ? 
-                   classement.map( (user , index) => {
-                    return (
-                        <Table.Row key = {index}>
-                            <Table.Cell>
-                                <span className="index_style_classement">
-                                {index+1}
-                                </span>
-                            </Table.Cell>
-                            
-                            
-                            <Table.Cell><span className="identifiants-classement-user"> {user.lastName} {user.firstName} </span><br></br>
-                            <span> {user.etablissement} </span>
-                            </Table.Cell>
-                            <Table.Cell  key={index}>
-                            <div className="high_fives_container">
-                                <span className="high_fives_span"> 
-                                    <img src={highfive} className="high_fives_images" alt = '' />
-                                 </span>
-                                <span className="numbers_high_five"> {user.points}</span> 
+                    {
+                        eleveFrance ? 
+                        eleveFrance.map( (user , index) => {
+                            return (
+                                <Table.Row key = {index}>
+                                    <Table.Cell>
+                                        <span className="index_style_classement">
+                                        {index+1}
+                                        </span>
+                                    </Table.Cell>
+                                    
+                                    
+                                    <Table.Cell><span className="identifiants-classement-user"> {user.lastName} {user.firstName} </span><br></br>
+                                    <span> {user.etablissement} </span>
+                                    </Table.Cell>
+                                    <Table.Cell  key={index}>
+                                    <div className="high_fives_container">
+                                        <span className="high_fives_span"> 
+                                            <img src={highfive} className="high_fives_images" alt = '' />
+                                        </span>
+                                        <span className="numbers_high_five"> {user.points}</span> 
+                                        
+                                    </div>
+                                    
+                                    </Table.Cell>
+                                </Table.Row>
                                 
-                            </div>
-                            
-                            </Table.Cell>
-                        </Table.Row>
+                            )
                         
-                    )
-                   
 
-                   }) : ''
-               }
+                        }) : ''
+                    }
+                    
+                    </Table.Body>
+                    </Table> 
+
+
+                </Tab>
+                <Tab eventKey="Lycées du Maroc" title="Lycées du Maroc">
+
+
+                        <Table unstackable>
+                        <Table.Body>
+
+                    {
+                        eleveMaroc ? 
+                        eleveMaroc.map( (user , index) => {
+                            return (
+                                <Table.Row key = {index}>
+                                    <Table.Cell>
+                                        <span className="index_style_classement">
+                                        {index+1}
+                                        </span>
+                                    </Table.Cell>
+                                    
+                                    
+                                    <Table.Cell><span className="identifiants-classement-user"> {user.lastName} {user.firstName} </span><br></br>
+                                    <span> {user.etablissement} </span>
+                                    </Table.Cell>
+                                    <Table.Cell  key={index}>
+                                    <div className="high_fives_container">
+                                        <span className="high_fives_span"> 
+                                            <img src={highfive} className="high_fives_images" alt = '' />
+                                        </span>
+                                        <span className="numbers_high_five"> {user.points}</span> 
+                                        
+                                    </div>
+                                    
+                                    </Table.Cell>
+                                </Table.Row>
+                                
+                            )
+                        
+
+                        }) : ''
+                    }
+                    
+                    </Table.Body>
+                    </Table> 
+
+
+                </Tab>
                
-               </Table.Body>
-            </Table>
+            </Tabs>
+
+           
             <div className="loader-container" style = {{ height: '30vh' }}>
                 {dimmer ? (<Dimmer active inverted>
                     <Loader inverted content='Chargement en cours...' />
