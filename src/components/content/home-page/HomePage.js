73de228ccf6a23f9d48d96dd04f1dbb02dc446f0
unknown <anashmidouch@gmail.com>
2020-12-03 17:46:49 +0100

    homepage lasy

--- src/components/content/home-page/HomePage.js
+++ src/components/content/home-page/HomePage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, memo, useEffect, useState,Suspense,lazy } from "react";
+import React, { Fragment, useContext, memo, useEffect, useState} from "react";
 import HeroSection from "./HeroSection";
 
 /* import FreeChapterSection from './FreeChapterSection/FreeChapterSection';*/
@@ -11,11 +11,11 @@ import UserContext from "../../../Context/UserContext/UserContext";
 import { Redirect } from "react-router-dom";
 import { Dimmer, Loader } from "semantic-ui-react";
 
-const SchoolInfosSection  =  lazy(() => import( "./SchoolInfosSection"));
-const ThirdDiv  =  lazy(() => import( "./Thirddiv"));
-const Sixthdiv  =  lazy(() => import( "./Sixthdiv"));
-const ApplicationSection  =  lazy(() => import( "./Application/ApplictaionSection"));
-const ClassesSection  =  lazy(() => import( "./ClassesSection/ClassesSection"));
+import SchoolInfosSection  from "./SchoolInfosSection";
+import ThirdDiv  from "./Thirddiv";
+import Sixthdiv  from "./Sixthdiv";
+import ApplicationSection  from "./Application/ApplictaionSection";
+import ClassesSection  from "./ClassesSection/ClassesSection";
 
 const HomePage = memo(({ loading }) => {
   const [redirect, setRedirect] = useState(false);
@@ -40,7 +40,7 @@ const HomePage = memo(({ loading }) => {
     return (
       <Fragment>
         <section id="homepage">
-        <Suspense fallback={<div></div>}>
+       
           <HeroSection userConnected={userContext.user} />
           <SchoolInfosSection />
           <ThirdDiv />
@@ -49,7 +49,7 @@ const HomePage = memo(({ loading }) => {
           <Sixthdiv />
           <ApplicationSection />
           <ClassesSection />
-        </Suspense>
+       
         </section>
         <Footer />
       </Fragment>
